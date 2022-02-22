966a3f339e007d636f5a1bae27430dbfd767cf6f
Gaurav Das <gaurav_ug@cse.nits.ac.in>
2022-02-22 23:37:31 +0530

    feat: update index

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = (app) => {
 
   app.on("issues.opened", async (context) => {
     const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
+      body: "Hey there, thanks for opening this issue!",
     });
     return context.octokit.issues.createComment(issueComment);
   });
